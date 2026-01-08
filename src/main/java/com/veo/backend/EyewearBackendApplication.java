69ded0702ce64a354d6d828730855f7c6a01e0a1
LongHB <huynhbaolong@gmail.com>
2026-01-08 09:07:43 +0700

    feat: init spring boot project structure

---
+++ src/main/java/com/veo/backend/EyewearBackendApplication.java
@@ -0,0 +1,13 @@
+package com.veo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyewearBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyewearBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veo/backend/EyewearBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyewearBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
