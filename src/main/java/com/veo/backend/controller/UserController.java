8b50e13f8070e0d17ba06a450ba1d88d346595d6
MACBOOK <phanquangtien1002@gmail.com>
2026-03-19 08:20:17 +0700

    Admin create user account

--- src/main/java/com/veo/backend/controller/UserController.java
+++ src/main/java/com/veo/backend/controller/UserController.java
@@ -1,19 +1,20 @@
 package com.veo.backend.controller;
 
+import com.veo.backend.dto.request.UserCreateRequest;
+import com.veo.backend.dto.response.UserCreateResponse;
+import com.veo.backend.service.AdminUserService;
 import com.veo.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final AdminUserService adminUserService;
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
@@ -25,6 +26,18 @@ public class UserController {
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public UserCreateResponse createUser(@RequestBody UserCreateRequest request) {
+        return adminUserService.createUser(request);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteUser(@PathVariable Long id) {
+        adminUserService.deleteUser(id);
     }
 
     @GetMapping("/profile")
---
+++ src/main/java/com/veo/backend/dto/response/UserCreateResponse.java
@@ -0,0 +1,18 @@
+package com.veo.backend.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserCreateResponse {
+    private Long id;
+
+    private String email;
+
+    private String fullName;
+
+    private String role;
+
+    private Boolean isActive;
+}
