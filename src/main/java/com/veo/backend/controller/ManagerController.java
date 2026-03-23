f50efd5263a816b1822e928448980f0780dfca4c
MACBOOK <phanquangtien1002@gmail.com>
2026-03-23 08:58:02 +0700

    Manager check staff list

--- src/main/java/com/veo/backend/controller/ManagerController.java
+++ src/main/java/com/veo/backend/controller/ManagerController.java
@@ -1,4 +1,34 @@
 package com.veo.backend.controller;
 
+import com.veo.backend.dto.response.UserResponse;
+import com.veo.backend.service.ManagerUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/manager/users")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('MANAGER')")
 public class ManagerController {
+    private final ManagerUserService managerUserService;
+
+    @GetMapping("/staff")
+    public ResponseEntity<List<UserResponse>> getStaffList() {
+        return ResponseEntity.ok(managerUserService.getStaffList());
+    }
+
+    @PatchMapping("/{userId}/{status}")
+    public ResponseEntity<String> toggleStatus(
+            @PathVariable Long userId,
+            @PathVariable boolean status) {
+
+        managerUserService.toggleUserStatus(userId, status);
+        String message = status ? "Unlocked account successfully" : "Locked account successfully";
+
+        return ResponseEntity.ok(message);
+    }
 }
--- src/main/java/com/veo/backend/repository/UserRepository.java
+++ src/main/java/com/veo/backend/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.veo.backend.repository;
 
 import com.veo.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +16,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserById(Long id);
 
     boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.role.name IN :roleNames")
+    List<User> findByRoleNames(@Param("roleNames") List<String> roleNames);
 }
